ea9e9062695fe921fe8022c6c52278d2a131712c
GORI16 <ohnoyusuke16@icloud.com>
2025-09-04 17:54:17 +0900

    update

---
+++ support.php
@@ -0,0 +1,44 @@
+<?php
+  include(__DIR__.'/include/header.php');
+?>
+<main>
+  <div class="container">
+    <h1>SUPPORT PLAYERS</h1>
+      <div class="support">
+        <li class="support_player">
+          <img src="/img/s_kirihata.jpg" alt="">
+          <div class="player_detail">
+            <h2>桐畑 和繁</h2>
+            <p>FC岐阜</p>
+              <ul>
+                <li><a href="https://www.instagram.com/k.kirihata/?hl=ja" class="fab fa-instagram fa-lg" target="blank"></a></li>
+              </ul>
+          </div>
+        </li>
+        <li class="support_player">
+          <img src="/img/s_kawanami.jpg" alt="">
+            <div class="player_detail">
+              <h2>川浪 吾郎</h2>
+              <p>サンフレッチェ広島</p>
+                <ul>
+                  <li><a href="https://www.instagram.com/goro_kawanami22.official/" class="fab fa-instagram fa-lg" target="blank"></a></li>
+                </ul>
+          </div>
+        </li>
+        <li class="support_player">
+          <img src="/img/s_matsumoto2.jpg" alt="">
+          <div class="player_detail">
+            <h2>松本 健太</h2>
+            <p>柏レイソル</p>
+              <ul>
+                <li><a href="https://www.instagram.com/mtkn_5_4_/?hl=ja" class="fab fa-instagram fa-lg" target="blank"></a></li>
+                <li><a href="https://twitter.com/matsu1997kenta" class="fab fa-twitter fa-lg" target="blank"></a></li>
+              </ul>
+          </div>
+        </li>
+      </div>
+  </div>
+</main>
+<?php
+  include(__DIR__.'/include/footer.php');
+?>
