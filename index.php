756bb1332ee41a2f4bc2e208c316a8102b523443
GORI16 <ohnoyusuke16@icloud.com>
2025-09-04 17:50:58 +0900

    update index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  include(__DIR__.'/include/header.php');
+?>
+<main>
+  <div class="container">
+    <div class="main-image">
+      <div class="image-left">
+        <div class="image-left-about">
+          <p class="about-top">FROM</p>
+          <p class="about-top-big">JAPAN</p>
+          <p class="about-top">TO THE</p>
+          <p class="about-top-big">WORLD</p>
+          <p class="about-top">世界を代表とする</p>
+          <p class="about-top">ゴールキーパーグローブへ</p>
+          <p><a href="/about.php" class="about-bottom" target="blank">→ ABOUT US</a></p>
+        </div>
+      </div>
+      <div>
+        <img src="/img/top_black.jpg" class="image-right">
+        <!-- <img src="/img/s_kirihata_2.jpg" class="image-right"> -->
+      </div>
+    </div>
+  </div>
+</main>
+<?php
+  include(__DIR__.'/include/footer.php');
+?>
