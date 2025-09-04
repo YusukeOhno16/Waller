86c6953bb53b7b4852463a9e1790b78f555fa4f9
GORI16 <ohnoyusuke16@icloud.com>
2025-09-04 17:54:46 +0900

    update

---
+++ include/footer.php
@@ -0,0 +1,23 @@
+<footer>
+  <div class="footer">
+      <div class="footer-menu">
+          <ul>
+            <li class="footer-menu-list"><a href="/../index">TOP</a></li>
+            <li class="footer-menu-list"><a href="/../product">PRODUCT</a></li>
+            <li class="footer-menu-list"><a href="/../about">ABOUT</a></li>
+            <li class="footer-menu-list"><a href="/../support">SUPPORT</a></li>
+            <li class="footer-menu-list"><a href="/../contact">CONTACT</a></li>
+            <li class="footer-menu-list"><a href="https://waller2020.thebase.in/" target=_blank>ONLINE SHOP</a></li>
+            <li class="footer-menu-list"><a href="https://www.instagram.com/waller_goalkeeper/?igshid=1erbiooohi8gr" class="fab fa-instagram fa-lg" target="blank"></a></li>
+            <li class="footer-menu-list"><a href="https://www.facebook.com/waller.goalkeeper/" class="fab fa-facebook fa-lg" target="blank"></a></li>
+            <li class="footer-menu-list"><a href="https://twitter.com/waller2020" class="fab fa-twitter fa-lg" target="blank"></a></li>
+          </ul>
+      </div>
+      <div class="footer-img">
+        <a href="/../"><img src="/img/WALLER-logo.png" alt="" srcset="" class="footer-img-logo"></a>
+      </div>
+      <div class="copyright">©︎ 2021 waller Co.,Ltd</div>
+  </div>
+</footer>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+  <title>WALLER/企業サイト</title>
+  <link href="style.css"rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+<header>
+  <div class="header">
+    <a href="/../"><div class="logo"></div></a>
+    <ul>
+      <a href="/../"><li>TOP</li></a>
+      <a href="/../product"><li>PRODUCT</li></a>
+      <a href="/../about"><li>ABOUT</li></a>
+      <a href="/../support"><li>SUPPORT</li></a>
+      <a href="/../contact" ><li>CONTACT</li></a>
+      <a href="https://waller2020.thebase.in/" target=_blank><li>ONLINE SHOP</li></a>
+      <li><a href="https://www.instagram.com/waller_goalkeeper/?igshid=1erbiooohi8gr" class="fab fa-instagram fa-lg" target="blank"></a></li>
+      <li><a href="https://www.facebook.com/waller.goalkeeper/" class="fab fa-facebook fa-lg" target="blank"></a></li>
+      <li><a href="https://twitter.com/waller2020" class="fab fa-twitter fa-lg" target="blank"></a></li>
+    </ul>
+    <div class="menuArea">
+      <a class="menu-trigger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+    </div>
+  </div>
+  <div class="menuWindow">
+  <ul>
+      <a href="/../"><li>TOP</li></a>
+      <a href="/../product"><li>PRODUCT</li></a>
+      <a href="/../about"><li>ABOUT</li></a>
+      <a href="/../contact"><li>CONTACT</li></a>
+      <a href="/../support"><li>SUPPORT</li></a>
+      <a href="https://waller2020.thebase.in/" target=_blank><li>ONLINE SHOP</li></a>
+      <li><a href="https://www.instagram.com/waller_goalkeeper/?igshid=1erbiooohi8gr" class="fab fa-instagram fa-lg" target="blank"> Instagram</a></li>
+      <li><a href="https://www.facebook.com/waller.goalkeeper/" class="fab fa-facebook fa-lg" target="blank"> Facebook</a></li>
+      <li><a href="https://twitter.com/waller2020" class="fab fa-twitter fa-lg" target="blank"> Twitter</a></li>
+    </ul>
+  </div>
+  <script type="text/javascript" src="common.js"></script>
+</header>
